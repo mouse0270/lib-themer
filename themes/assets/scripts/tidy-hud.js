04e03e0ca1da7deb7be64341e0e6b7929e71874d
Robert McIntosh <rmcintosh@remabledesigns.com>
2022-09-18 14:35:44 -0400

    v0.1.0f
    updated some theme options and added notifications to TidyHUD

--- themes/assets/scripts/tidy-hud.js
+++ themes/assets/scripts/tidy-hud.js
@@ -4,8 +4,18 @@ const TidyHUD = {
 			elemPlayer.dataset.tooltip = elemPlayer.textContent;
 			elemPlayer.dataset.tooltipDirection = "RIGHT";
 		});
+	},
+	collapseSidebar: async (app, collapsed) => {
+		if (collapsed) {
+			const position = parseFloat(document.querySelector(":root").style.getPropertyValue('--sidebar-width')) - document.querySelector('#sidebar').offsetWidth
+			document.querySelector('#notifications').style.marginRight = `-${position}px`;
+		}else{
+			document.querySelector('#notifications').style.marginRight = '0px';
+		}
 	}
 };
 
 Hooks.on('renderPlayerList', TidyHUD.renderPlayerList);
+Hooks.on('collapseSidebar', TidyHUD.collapseSidebar);
 TidyHUD.renderPlayerList({}, [document.querySelector('aside#players')], {});
+TidyHUD.collapseSidebar([document.querySelector('#sidebar')], document.querySelector('#sidebar').classList.contains('collapsed'));
